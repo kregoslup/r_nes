256b43dcad952813928d301861127cccf671377b
krego <mateusz.olczynski@gmail.com>
2020-05-17 15:13:32 +0200

    Add loading prg and chr

--- src/cartridge.rs
+++ src/cartridge.rs
@@ -1,4 +1,4 @@
-use crate::util::{combine_nibbles};
+use crate::util::{combine_nibbles, nth_bit};
 
 pub struct Cartridge {
     prg_rom: Vec<u8>,
@@ -6,12 +6,17 @@ pub struct Cartridge {
     mapper: u8
 }
 
-impl Cartridge {
+pub struct CartridgeLoader {
+    payload: Vec<u8>
+}
+
+impl CartridgeLoader {
     pub fn load_cartridge(payload: Vec<u8>) -> Cartridge {
-        Cartridge::assert_constant(&payload);
-        let mapper = Cartridge::load_mapper(&payload);
-        let prg_rom = Cartridge::load_prg(&payload);
-        let chr_rom = Cartridge::load_chr(&payload);
+        let mut loader = CartridgeLoader { payload };
+        loader::assert_constant(&payload);
+        let mapper = loader.load_mapper();
+        let prg_rom = loader.load_prg();
+        let chr_rom = loader.load_chr();
         return Cartridge {
             prg_rom,
             chr_rom,
@@ -19,33 +24,58 @@ impl Cartridge {
         }
     }
 
-    fn assert_constant(payload: &Vec<u8>) {
+    fn assert_constant(&mut self) {
         let header_constant_start = 0;
         let header_constant_end = 3;
         let header_constant_combination: Vec<u8> = vec![0x4E, 0x45, 0x53, 0x1A];
-        let valid_header = payload[header_constant_start..header_constant_end] == *header_constant_combination;
+        let valid_header = self.payload[header_constant_start..header_constant_end] == *header_constant_combination;
         if !valid_header {
             panic!("ROM does not contain the usual header");
         }
     }
 
-    fn load_mapper(payload: &Vec<u8>) -> u8 {
+    fn load_mapper(&mut self) -> u8 {
         let lower_mapper_flag = 6;
         let upper_mapper_flag = 7;
-        let lower_nibble = (payload[lower_mapper_flag] & 0x10) >> 4;
-        let upper_nibble = payload[lower_mapper_flag] & 0x10;
+        let lower_nibble = (self.payload[lower_mapper_flag] & 0x10) >> 4;
+        let upper_nibble = self.payload[lower_mapper_flag] & 0x10;
         return lower_nibble | upper_nibble;
     }
 
-    fn load_prg(payload: &Vec<u8>) -> Vec<u8> {
-        let prg_start = 2 + 64; // HEADER - 16 bytes + Trainer 512 BYTES
-        let prg_rom_size_flag = 4;
-        let size = payload[prg_rom_size_flag] * 2; // Size provided in 16 kB units;
-        return payload[prg_start..(prg_start + size)]
+    fn prg_size(&mut self) -> u8 {
+        let prg_rom_size_bit = 4;
+        return self.payload[prg_rom_size_flag] * 2;
     }
 
-    fn load_chr(payload: &Vec<u8>) -> Vec<u8> {
+    fn chr_size(&mut self) -> u8 {
+        let chr_size_flag = 5;
+        return self.payload[chr_size_flag]
+    }
 
+    fn trainer_offset(&mut self) -> u8 {
+        let trainer_flag = 6;
+        let has_trainer = nth_bit(self.payload[trainer_flag], 3);
+        return if has_trainer() {
+            512
+        } else {
+            0
+        }
+    }
+
+    fn load_prg(&mut self) -> Vec<u8> {
+        let header_offset = 16;
+        let prg_start = header_offset + self.trainer_offset(); // HEADER - 16 bytes + Trainer 512 BYTES
+        let size = self.prg_size();
+        return self.payload[prg_start..(prg_start + size)]
+    }
+
+    fn load_chr(&mut self) -> Vec<u8> {
+        let header_offset = 16;
+        let trainer_offset = self.trainer_offset();
+        let prg_offset = self.prg_size();
+        let chr_size = self.chr_size();
+        let chr_start = header_offset + trainer_offset + prg_offset;
+        return self.payload[chr_start..(chr_start + chr_size)]
     }
 }
 
@@ -55,7 +85,7 @@ mod tests {
 
     #[test]
     fn test_load_cartridge() {
-        let cartridge = Cartridge::load_cartridge(vec![0, 0, 0, 0, 0x91, 0x82]);
+        let cartridge = CartridgeLoader::load_cartridge(vec![0, 0, 0, 0, 0x91, 0x82]);
         assert_eq!(cartridge.mapper, 0x12)
     }
 }
